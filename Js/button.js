282124c1b3cee0036af446e7e33dce1d0311778c
ToshiyaMatsuoka <triger.1306@gmail.com>
2019-07-15 13:18:46 +0900

    レイアウト変更

--- Js/button.js
+++ Js/button.js
@@ -1,4 +1,4 @@
-window.onload = ()=>{
+window.addEventListener('DOMContentLoaded', ()=>{
 
     let obj = document.querySelector("#backTop");
     obj.addEventListener("click", 
@@ -6,17 +6,17 @@ window.onload = ()=>{
         scrollTo(0,0);
     }
     );
+});
+window.onscroll = ()=>{    
+        // let obj = window.querySelector("body");
+window.addEventListener("scroll",()=>{
+ if(window.scrollY <=0){
+     document.getElementById("backTop").style.animationName="fadeOut"
+    document.getElementById("backTop").style.display="none";
+  }
+  else{ 
+      document.getElementById("backTop").style.animationName="fadeIn"
+    document.getElementById("backTop").style.display="block";
+  }
+});        
 }
-// window.onscroll = ()=>{    
-//         // let obj = window.querySelector("body");
-// window.addEventListener("scroll",()=>{
-//  if(window.scrollY <=0){
-//      document.getElementById("backTop").style.animationName="fadeOut"
-//     document.getElementById("backTop").style.display="none";
-//   }
-//   else{ 
-//       document.getElementById("backTop").style.animationName="fadeIn"
-//     document.getElementById("backTop").style.display="block";
-//   }
-// });        
-// }
